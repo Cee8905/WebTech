0d3cdf9dc0ee098f3614c95a9a1089888cfa872e
Qiankai48 <82523945+Qiankai48@users.noreply.github.com>
2021-10-22 15:23:40 +0200

    Update Ticket.java
    ticket update, alignment with db

--- bus-verwaltung-war/src/beans/Ticket.java
+++ bus-verwaltung-war/src/beans/Ticket.java
@@ -6,15 +6,24 @@ public class Ticket implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Long ticket_id;
-	private String 	category;
-	private String 	parameters;
-	private String 	fb_oc;
+	private String username;
+	private String category;
+	private String parameters;
+	private String message;
+	private String priority;
+	private String status;
 	
 	public Long getTicket_id() {
 		return ticket_id;
 	}
 	public void setTicket_id(Long ticket_id) {
 		this.ticket_id = ticket_id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
 	}
 	public String getCategory() {
 		return category;
@@ -28,11 +37,23 @@ public class Ticket implements Serializable {
 	public void setParameters(String parameters) {
 		this.parameters = parameters;
 	}
-	public String getFb_oc() {
-		return fb_oc;
+	public String getPriority() {
+		return priority;
 	}
-	public void setFb_oc(String fb_oc) {
-		this.fb_oc = fb_oc;
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
 	}
 	
 }
