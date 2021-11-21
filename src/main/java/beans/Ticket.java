7c8f32ab9e4940ec6cb90895b3297af9f40a9f3d
Qiankai48 <82523945+Qiankai48@users.noreply.github.com>
2021-11-21 14:46:09 +0100

    Update Ticket.java

--- src/main/java/beans/Ticket.java
+++ src/main/java/beans/Ticket.java
@@ -12,60 +12,48 @@ public class Ticket implements Serializable {
 	private String message;
 	private String priority;
 	private String status;
-
+	
 	public Long getTicket_id() {
 		return ticket_id;
 	}
-
 	public void setTicket_id(Long ticket_id) {
 		this.ticket_id = ticket_id;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 	public String getCategory() {
 		return category;
 	}
-
 	public void setCategory(String category) {
 		this.category = category;
 	}
-
 	public String getParameters() {
 		return parameters;
 	}
-
 	public void setParameters(String parameters) {
 		this.parameters = parameters;
 	}
-
 	public String getPriority() {
 		return priority;
 	}
-
 	public void setPriority(String priority) {
 		this.priority = priority;
 	}
-
 	public String getStatus() {
 		return status;
 	}
-
 	public void setStatus(String status) {
 		this.status = status;
 	}
-
 	public String getMessage() {
 		return message;
 	}
-
 	public void setMessage(String message) {
 		this.message = message;
 	}
+	
 }
